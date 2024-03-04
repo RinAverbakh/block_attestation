652722ff456bb1d20e69a207771e95d5a610790d
Rin <arina.averbakh.st@gmail.com>
2024-03-04 14:00:04 +0300

    была написана программа, решающая поставленную задачу

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int CalculateElementsNewSrings (string [] strings)
+{
+    int i = 0;
+    int count = 0;
+    while (i < strings.Length)
+    {
+        if (strings[i].Length < 4)
+        {
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
+
+string [] CreateNewStrings (string [] strings, int count)
+{
+    string [] newStrings = new string [count];
+    int i = 0;
+    int j = 0;
+    while (i < strings.Length)
+    {
+        if (strings[i].Length < 4)
+        {
+            newStrings[j] = strings[i];
+            j++;
+        }
+        i++;
+    }
+    return newStrings;
+}
+
+string[] start = ["Hello", "2", "world", ":-)"];
+string[] finish = CreateNewStrings(start, CalculateElementsNewSrings(start));
+Console.WriteLine("[" + string.Join(", ", finish) + "]");
